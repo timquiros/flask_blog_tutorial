19aae75cf940fa6d53288af71a77f2c6092aa883
Timothy Quiros <quirostim@gmail.com>
2020-05-30 23:11:21 +0800

    add forms classes and set app routes to it

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistraionForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '93733d55670b2c9ed718ff5a44501a2d' 
 
 posts = [
 	{
@@ -24,6 +27,13 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title="About")
+
+@app.route('/register')
+	form = RegistraionForm()
+
+@app.route('/login')
+	form = LoginForm()
+	return render_template("login.html", title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
